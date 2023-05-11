5a8d38106224e790833bbe01a3ac7ce5ccd8edcc
rosesandbooks89 <rosesandbooks89@gmail.com>
2023-05-10 20:26:23 -0500

    pushing to have code in repos

--- assets/script.js
+++ assets/script.js
@@ -4,32 +4,18 @@ var timer;
 var seconds = 10;
 var startScreen = document.querySelector("#start-screen");
 
-//questions
-const question1 = new Question(
-  `How are JavaScript single line comments started?`,
-  [`/*`, `<!--`, `//`],
-  correctAnswer: 2
-);
-const question2 = new Question(
-  `Which term(s) were introduced as a preferred replacement to 'var' in ECMA2015?`,
-  [`'let' and 'const'`, `'break' and 'continue'`, `while`, `switch`],
-  0
-);
-const question3 = new Question(
-  `Which of the following is a JavaScript Event type?`,
-  [`load`, `mouseover`, `keydown`, `all of the above`],
-  3
-);
-const question4 = new Question(
-  `What is the structure of an arrow function expression for binding the function 'add'?`,
-  [
-    `const add = {a,b} => (a + b)`,
-    `const add = (a,b) => {a + b}`,
-    `function add => (a,b) = {a + b}`,
-    `add(a,b) = {a + b}`,
-  ],
-  1
-);
+const questions = [
+    {
+        question: "Example question",
+        answers: [
+            "Answer1",
+            "Answer2",
+            "Answer3",
+            "Answer4"
+        ]
+    }
+]
+
 
 function startGame() {
   // start timer (local function)
